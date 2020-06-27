a9fcc71240654be0b63581946138af824e2b879f
surelis segarra <sdarianne34@gmail.com>
2020-06-27 15:46:16 -0700

    can see channels in db and state but not in the browser

--- src/Components/SidePanel/Channels.js
+++ src/Components/SidePanel/Channels.js
@@ -13,6 +13,18 @@ export default class Channels extends Component {
         modal: false,
     };
 
+    componentDidMount() {
+        this.addListeners();
+    }
+
+    addListeners = () => {
+        let loadedChannels = [];
+        this.state.channelsRef.on('child_added', snap => {
+            loadedChannels.push(snap.val());
+            this.setState({ channels: loadedChannels });
+        })
+    }
+
     addChannel = () => {
         const { channelsRef, channelName, channelDetails, user } = this.state;
 
@@ -53,6 +65,19 @@ export default class Channels extends Component {
         this.setState({ [e.target.name]: e.target.value });
     };
 
+    displayChannels = channels  => {
+        channels.length > 0 && channels.map(channel => (
+            <Menu.Item
+                key={channel.id}
+                onClick={() => console.log(channel)}
+                name={channel.name}
+                style={{ opacity: 0.7 }}
+            >
+                # {channel.name}
+            </Menu.Item>
+        ))
+    }
+
     isFormValid = ({ channelName, channelDetails }) => channelName && channelDetails;
 
     openModal = () => this.setState({ modal:true });
@@ -72,6 +97,7 @@ export default class Channels extends Component {
                     ({ channels.length }) <Icon name='add' onClick={this.openModal}/>
                     </MenuItem>
                     {/* channels */}
+                {this.displayChannels(channels)}
                 </Menu.Menu>
 
                 {/* add channel modal */}
