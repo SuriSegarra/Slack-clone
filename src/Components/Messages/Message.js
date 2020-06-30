aadf718cc9d06d3098a6bf7731ea411d58b93866
surelis segarra <sdarianne34@gmail.com>
2020-06-30 16:06:36 -0700

    created message component and now messages are displayed

---
+++ src/Components/Messages/Message.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import moment from 'moment';
+import { Comment, CommentAvatar, CommentContent, CommentAuthor, CommentMetadata, CommentText } from 'semantic-ui-react';
+
+// determine whether the ID of the user thats currently is the same as the ID of the user that created the message 
+//if those 2 ids are the same will give it a class of message self otherwise no class 
+const isOnMessage = (message, user) => {
+    return message.user.id === user.uid ? 'message__self' : '';
+};
+
+// it will taje the message time stamp whrn the message was created and converted it into a time from the current moment
+const timeFromNow = timestamp => moment(timestamp).fromNow();
+
+export default function Message({ message, user }) {
+    return (
+        <Comment>
+            <CommentAvatar src={message.user.avatar}/>
+            <CommentContent className={isOnMessage(message, user)}>
+            <CommentAuthor as='a'>{message.user.name}</CommentAuthor>
+            <CommentMetadata>{timeFromNow(message.timestamp)}</CommentMetadata>
+            <CommentText>{message.content}</CommentText>
+            </CommentContent>
+        </Comment>
+    )
+}
--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
+import Message from './Message';
 import { Segment, Comment } from 'semantic-ui-react';
 import firebase from '../../firebase';
 
 export default class Messages extends Component {
     state = {
         messagesRef: firebase.database().ref('messages'),
+        messages: '',
+        messagesLoading: true,
         channel: this.props.currentChannel,
-        user: this.props.currentUser
+        user: this.props.currentUser,
 
+    };
+    // TO check if we have a value in our gs for currentChannel and currentUser 
+    componentDidMount() {
+        const { channel, user } = this.state;
+
+        if(channel && user) {
+            this.addListeners(channel.id);
+        }
+    }
+
+    addListeners = channelId => {
+        this.addMessageListener(channelId);
+    }
+// we're going to listen for any new messages added for a given channel 
+    addMessageListener = channelId => {
+        let loadedMessages = [];
+        this.state.messagesRef.child(channelId).on('child_added', snap => {
+            loadedMessages.push(snap.val());
+            this.setState({
+                messages: loadedMessages,
+                messagesLoading: false 
+            });
+        });
+    };
+
+    displayMessages = messages => {
+      return  messages.length > 0 && messages.map(message => (
+            <Message
+                key={message.timestamp}
+                message={message}
+                user={this.state.user}
+            />
+        ));
     };
 
     render() {
-        const { messagesRef, channel, user } = this.state;
+        const { messagesRef, messages, channel, user } = this.state;
 
         return (
             <React.Fragment>
@@ -21,7 +57,7 @@ export default class Messages extends Component {
 
                 <Segment>
                     <Comment.Group className='messages'>
-
+                    {this.displayMessages(messages)}
                     </Comment.Group>
                 </Segment>
 
