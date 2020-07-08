8091b5ad09fa9772b1d1bc38aa4cf6dcb40265f2
surelis segarra <sdarianne34@gmail.com>
2020-07-08 10:55:21 -0700

    created DirectMessages component

---
+++ src/Components/SidePanel/DirectMessages.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DirectMessages extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
