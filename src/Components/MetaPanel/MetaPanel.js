b22839aa00812c4e0232ee23b097a85742f75cb7
surelis segarra <sdarianne34@gmail.com>
2020-07-13 14:58:58 -0700

    scaffold metaPanel component. We can see accordions on broser

--- src/Components/MetaPanel/MetaPanel.js
+++ src/Components/MetaPanel/MetaPanel.js
@@ -1,11 +1,71 @@
-import React, { Component } from 'react'
+// includes info about the channel that users currently on
+import React, { Component } from 'react';
+import { Segment, Accordion, Header, Icon } from 'semantic-ui-react';
 
 export default class MetaPanel extends Component {
+    state = {
+        activeIndex: 0
+    };
+
+    // when we click on a title, we're going to set the active index and therefore (por lo tanto)
+    // give us access to the index that we set on title 
+    setActiveIndex = (event, titleProps) => {
+        const { index } = titleProps;
+        const { activeIndex } = this.state;
+        // give value of -1 otherwise the index thats being passed when we clikc a give part of the accordion
+        const newIndex = activeIndex === index ? -1 : index;
+        this.setState({ activeIndex: newIndex });
+    };
+
     render() {
+        const { activeIndex } = this.state;
+
         return (
-            <div>
-                MetaPanel
-            </div>
+           <Segment>
+               <Header as='h3' attached='top'>
+                   About the # Channel
+               </Header>
+               <Accordion styled attached='true'>
+                <Accordion.Title
+                active={activeIndex === 0}
+                index={0}
+                onClick={this.setActiveIndex}
+                >
+                    <Icon name='dropdown' />
+                    <Icon name='info' />
+                    Channel Details
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 0}>
+                    details 
+                </Accordion.Content>
+
+                <Accordion.Title
+                active={activeIndex === 1}
+                index={1}
+                onClick={this.setActiveIndex}
+                >
+                    <Icon name='dropdown' />
+                    <Icon name='user cirlce' />
+                    Top Posters 
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 1}>
+                    posters
+                </Accordion.Content>
+
+                <Accordion.Title
+                active={activeIndex === 2}
+                index={2}
+                onClick={this.setActiveIndex}
+                >
+                    <Icon name='dropdown' />
+                    <Icon name='pencil alternate' />
+                   Created By 
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 2}>
+                    creator 
+                </Accordion.Content>
+               </Accordion>
+           </Segment>
         )
     }
 }
