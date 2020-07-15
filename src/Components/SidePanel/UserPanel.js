968e053500e21f673881155d69b684457a0ff64e
surelis segarra <sdarianne34@gmail.com>
2020-07-14 17:16:53 -0700

    we can see modal and cancel button works

--- src/Components/SidePanel/UserPanel.js
+++ src/Components/SidePanel/UserPanel.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import firebase from '../../firebase';
-import { Grid, GridColumn, GridRow, Header, HeaderContent, Icon, Dropdown, Image } from 'semantic-ui-react';
+import { Grid, GridColumn, GridRow, Header, HeaderContent, Icon, Dropdown, Image, Modal, Input, Button } from 'semantic-ui-react';
 
 class UserPanel extends React.Component {
-
     state = {
-        user: this.props.currentUser
-    }
+        user: this.props.currentUser,
+        modal: false
+    };
+
+    openModal = () => this.setState({ modal: true });
+
+    closeModal = () => this.setState({ modal: false });
 
     dropdownOptions = () => [
         {
@@ -19,7 +23,7 @@ class UserPanel extends React.Component {
         },
         {
             key: 'avatar',
-            text: <span>Change Avatar</span>
+            text: <span onClick={this.openModal}>Change Avatar</span>
         },
         {
             key: 'sign out',
@@ -36,7 +40,7 @@ firebase
     } 
     render() {
 
-        const { user } = this.state;
+        const { user, modal } = this.state;
         const { primaryColor } = this.props
       
         return (
@@ -55,11 +59,47 @@ firebase
                                 trigger={
                                 <span>
                                 <Image src={user.photoURL} spaced='right' avatar/>
-                                {user.displayName}</span>} 
+                                {user.displayName}</span>
+                                } 
                                 options={this.dropdownOptions()}
                             />
                         </Header>
                     </GridRow>
+
+                    {/* Change User Avatar Modal */}
+                    <Modal basic open={modal} onClose={this.closeModal}>
+                        <Modal.Header> Change Avatar </Modal.Header>
+                        <Modal.Content>
+                            <Input  
+                                fluid
+                                type='file'
+                                label='New Avatar'
+                                name='previewImage'
+                            />
+                            <Grid centered stackable columns={2}>
+                                <GridRow centered>
+                                    {/* smenatic ui class */}
+                                    <GridColumn className='ui center aligned grid'>
+                                        {/* image preview */}
+                                    </GridColumn>
+                                    <GridColumn>
+                                        {/* cropped image preview  */}
+                                    </GridColumn>
+                                </GridRow>
+                            </Grid>
+                        </Modal.Content>
+                        <Modal.Actions>
+                            <Button color='green' inverted>
+                                <Icon name='save' /> Change Avatar
+                            </Button>
+                            <Button color='green' inverted>
+                                <Icon name='image' /> Preview
+                            </Button>
+                            <Button color='red' inverted onClick={this.closeModal}>
+                                <Icon name='remove' /> Cancel
+                            </Button>
+                        </Modal.Actions>
+                    </Modal>
                 </GridColumn>
             </Grid>
         )
