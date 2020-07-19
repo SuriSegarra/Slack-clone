36934376ef0c4a33d8700e05a5fefbc8c6ec37c1
surelis segarra <sdarianne34@gmail.com>
2020-07-19 12:51:54 -0700

    you can add more text without hacing to click on the input

--- src/Components/Messages/MessageForm.js
+++ src/Components/Messages/MessageForm.js
@@ -60,6 +60,7 @@ export default class MessageForm extends Component {
         const oldMessage = this.state.message;
         const newMessage = this.colonToUnicode(`${oldMessage}${emoji.colons} `)
         this.setState({ message: newMessage, emojiPicker: false });
+        setTimeout(() => this.messageInputRef.focus(), 0);
     };
 
     colonToUnicode = message => {
@@ -218,8 +219,14 @@ export default class MessageForm extends Component {
                 onChange={this.handleChange}
                 onKeyDown={this.handleKeyDown}
                 value={message}
+                ref={node => (this.messageInputRef = node)}
                 style={{ marginBottom: '0.7em' }}
-                label={<Button icon={'add'} onClick={this.handleTogglePicker}/>}
+                label={
+                    <Button 
+                        icon={emojiPicker ? 'close' : 'add'}
+                        content={emojiPicker ? 'Close' : null} 
+                        onClick={this.handleTogglePicker}
+                    />}
                 labelPosition='left'
                 className={
                     errors.some(error => error.message.includes('message')) ? 'error' : ''
